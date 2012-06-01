892f219c26a40a0ef19e9f3b685644ddfc460ebd
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-06-01 10:42:29 +0200

    Remove .comment-nav-aboove and -below, make code less unreadable.

--- comments.php
+++ comments.php
@@ -28,18 +28,13 @@
 	<?php if ( have_comments() ) : ?>
 		<h2 id="comments-title">
 			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentyeleven' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			printf(_n( 'One thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						get_comments_number(), 'twentyeleven' ),
+					number_format_i18n( get_comments_number() ),
+					'<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'twentyeleven' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyeleven' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
 
 		<ol class="commentlist">
 			<?php
@@ -53,23 +48,15 @@
 			?>
 		</ol>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'twentyeleven' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyeleven' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
-
 	<?php
-		/* If there are no comments and comments are closed, let's leave a little note, shall we?
-		 * But we don't want the note on pages or post types that do not support comments.
-		 */
-		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'twentyeleven' ); ?></p>
+	# If no comments and comments are closed, leave a note.
+	# Skip on pages or post types that do not support comments.
+	#
+	elseif (!comments_open() && !is_page() &&
+			post_type_supports(get_post_type(), 'comments')) : ?>
+		<p class="nocomments"><?php _e('Comments are closed.', 'twentyeleven'); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
 
-</div><!-- #comments -->
+</div>
