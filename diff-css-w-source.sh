7d5a3835e828ddb527b29989b16b7b0923aa9f62
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-06-18 14:14:51 +0200

    Split util scripts into `target` and `source` parts.

---
+++ diff-css-w-source.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+diff ~/me-dev/debiki/all/debiki-app-play/public/res/debiki.css res/debiki.css
---
+++ sync-overwrite-css-w-source.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ~/me-dev/debiki/all/debiki-app-play/public/res/debiki.css res/debiki.css
+
