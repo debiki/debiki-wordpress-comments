ca9c3d30a512b456dd551e2d668784b0c23ce6ad
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-06-20 18:19:52 +0200

    Place a placeholder when moving blog post reply form
    So all comments won't jump to the left, which they would have done had
    the gap left by the reply form not been filled by these placeholders.

--- theme-specific/twentyeleven-v0-script.js
+++ theme-specific/twentyeleven-v0-script.js
@@ -15,10 +15,35 @@ jQuery(function($) {
   var $lastReplyLinkClicked = $();
   var $cancelCommentReplyLink = $('#cancel-comment-reply-link');
 
+  // When the reply form is moved to elsewhere, these placeholders
+  // are placed at the location of the reply form before it was moved.
+  // Then all comments won't jump upwards/leftwards suddenly
+  // (which they would have done, had the gap left by the reply form
+  // not been filled by these placeholders).
+  // (Their size exactly match the size of the reply form.)
+  var $blogPostReplyFormPlaceholder = undefined;
+  //var $commentReplyFormPlaceholder =
+    //  $('<div id="dw-wp-comment-reply-form-placeholder"></div>');
+
+  function showBlogPostReplyFormPlaceholder() {
+    if (!$blogPostReplyFormPlaceholder) {
+      $blogPostReplyFormPlaceholder = $replyForm.clone();
+      $blogPostReplyFormPlaceholder.find('[id]').removeAttr('id');
+      var $articleReplyList = $('.dw-depth-0 > .dw-res');
+      $blogPostReplyFormPlaceholder
+          .css('visibility', 'hidden')
+          .prependTo($articleReplyList);
+    }
+
+    $blogPostReplyFormPlaceholder.show();
+  }
+
+  function hideBlogPostReplyFormPlaceholder() {
+    $blogPostReplyFormPlaceholder.hide();
+  }
+
   $('.comment-reply-link').removeAttr('onclick').click(function(event) {
     var $i = $(this);
-
-    $cancelCommentReplyLink.fadeIn();
 
     // Hide the 'Reply' link that the user just clicked, because it's
     // pointless to show a 'Reply' link just above the reply form.
@@ -27,8 +52,15 @@ jQuery(function($) {
     $lastReplyLinkClicked.hide();
 
     // Move the reply form.
+    showBlogPostReplyFormPlaceholder();
     var $commentToReplyTo = $i.closest('.dw-p');
-    $replyForm.hide().insertAfter($commentToReplyTo).slideDown('slow');
+    $replyForm.hide().insertAfter($commentToReplyTo)
+        .slideDown(1100, function() {
+      $cancelCommentReplyLink.fadeIn();
+      // The $cancelCommentReplyLink is placed *above* the form, so it won't
+      // appear unless:
+      $replyForm.css('overflow', 'visible');
+    });
 
     event.preventDefault();
   });
@@ -38,6 +70,7 @@ jQuery(function($) {
     $lastReplyLinkClicked.fadeIn();
 
     $replyForm.slideUp(function() {
+      hideBlogPostReplyFormPlaceholder();
       $(this).prependTo($replyFormOriginalParent).fadeIn();
     });
 
