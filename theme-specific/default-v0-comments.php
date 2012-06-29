8a555d241e03adfe3a6feb9e67f9a15bd940a79f
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-06-29 22:29:31 +0200

    Improve the default theme.

--- theme-specific/default-v0-comments.php
+++ theme-specific/default-v0-comments.php
@@ -1,18 +1,16 @@
 <?php
 /**
+ * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+ * Licensed: GPL v2 or later
+ *
  * Parts Copyright (c) 2011 by the contributors (to WordPress and (?) b2,
  * some of the people mentioned here, I (KajMagnus, see below) suppose:
  *   http://codex.wordpress.org/Copyright_Holders  )
  * The contributors' code is licensed under GNU General Public License v2 or later,
  * see: wp-content/themes/twentyeleven/style.css
  *
- * Parts Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
- * Licensed: GPL v2 or later
- *
- *
- * I (KajMagnus) have edited the PHP code to output certain Debiki related CSS classes,
- * and some extra tags, and removed next and previous comment page links (which makes
- * no sense with Debiki's two dimensional layout).
+ * The contributors (see above) have copyright on the messages inside `_e()` and
+ * `_n()` and `__()`.
  */
 
 
@@ -20,60 +18,64 @@
 # Comment section template
 #===========================================================
 
-/**
- * Based on [wordpress-3.3.2]/wp-content/themes/twentyeleven/comments.php
- */
+?>
 
-# `.entry-content` aligns it with, and makes it as wide as, the article ?>
-<div class='entry-content'>
-<div id='comments' class='debiki dw-debate dw-wp-hide-reply-link'>
-<div id="dw-t-1" class="dw-t dw-ar-t dw-depth-0 dw-hor dw-svg-gparnt">
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
-	</div><!-- #comments -->
+<section class='comments dw-wp-comment-section'>
+
+<?php
+if (post_password_required()) :
+?>
+	<p class='nopassword'>
+	<?php _e('This post is password protected. Enter the password to view any comments.', 'twentyeleven'); ?>
+	</p>
+</section>
+<?php
+	return;
+endif;
+
+if (have_comments() ) :
+?>
+
+	<h2 id='dw-wp-comments-title'>
+		<?php
+		$formatStr = _n(
+				'One thought on &ldquo;%2$s&rdquo;',
+				'%1$s thoughts on &ldquo;%2$s&rdquo;',
+				get_comments_number(),
+				'twentyeleven');
+		$prettyCommentCount = number_format_i18n(get_comments_number());
+		printf($formatStr, $prettyCommentCount,
+				'<span>'.get_the_title().'</span>');
+		?>
+	</h2>
+
+	<div class='debiki dw-debate'>
+	<div id='dw-t-1' class='dw-t dw-depth-0 dw-hor dw-svg-gparnt'>
+	<div class='dw-p'></div>
+	<div class='dw-t-vspace'></div>
+	<ol class='dw-res'>
+		<?php
+		comment_form();
+		debiki_list_comments('debiki_default_comment');
+		?>
+	</ol>
+	</div>
+	</div>
 	<?php
-			/* Stop the rest of comments.php from being processed,
-			 * but don't kill the script entirely -- we still have
-			 * to fully load the template.
-			 */
-			return;
-		endif;
-	?>
+endif;
 
-	<?php // You can start editing here -- including this comment! ?>
+# If no comments and comments are closed, leave a note.
+# Skip on pages or post types that do not support comments.
+if (!comments_open() && !is_page() &&
+		post_type_supports(get_post_type(), 'comments')) :
+?>
+	<p class='nocomments'>
+		<?php _e('Comments are closed.', 'twentyeleven'); ?>
+	</p>
+<?php
+endif; ?>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 id="comments-title">
-			<?php
-			printf(_n( 'One thought on &ldquo;%2$s&rdquo;',
-						'%1$s thoughts on &ldquo;%2$s&rdquo;',
-						get_comments_number(), 'twentyeleven' ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
-
-		<!-- Makes debiki.js find the parent().children('.dw-t-vspace') and create an SVG root. -->
-		<div class='dw-p'></div>
-		<div class='dw-t-vspace'></div>
-		<ol class="commentlist dw-res ui-helper-clearfix">
-			<?php
-			comment_form();
-			debiki_list_comments('debiki_twenty_eleven_comment');
-			?>
-		</ol>
-	<?php
-	# If no comments and comments are closed, leave a note.
-	# Skip on pages or post types that do not support comments.
-	#
-	elseif (!comments_open() && !is_page() &&
-			post_type_supports(get_post_type(), 'comments')) : ?>
-		<p class="nocomments"><?php _e('Comments are closed.', 'twentyeleven'); ?></p>
-	<?php endif; ?>
-
-</div>
-</div>
-</div>
+</section>
 <?php
 
 
@@ -83,21 +85,14 @@
 #===========================================================
 
 /**
- * Adds these classes to comment related tags:
- *	  dw-t, dw-depth-<x>, dw-p, dw-p-hd, dw-p-bd
+ * Similar to Twenty Eleven's `twentyeleven_comment()`, but adds some new tags
+ * and CSS classes and a fold thread button: [–].
  *
- * And adds a fold thread button: [–]  or unfold (if folded): [+]
- *
- * Otherwise copied from function twentyeleven_comment(..) in
- * wp-content/themes/twentyeleven/functions.php. I've tried to do as few edits as
- * possible, so a diff gives meaningful results.
- *
- * (There's a filter, 'comment_class', that I could use to add 'dw-t' and 'dw-depth-'.
- * However more classes needs to be added elsewhere. And anyway I don't think it's
- * a good idea to depend on code in the Twenty-Eleven theme -- then Debiki's code
- * could suddenly break, should someone upgrade the theme.)
+ * Initially copied from function `twentyeleven_comment()` in
+ * wp-content/themes/twentyeleven/functions.php. I've tried to do few edits,
+ * so a diff should give meaningful results.
  */
-function debiki_twenty_eleven_comment( $comment, $args, $depth ) {
+function debiki_default_comment( $comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 	switch ( $comment->comment_type ) :
 		case 'pingback' :
@@ -109,51 +104,61 @@ function debiki_twenty_eleven_comment( $comment, $args, $depth ) {
 			break;
 		default :
 	?>
-	<li <?php comment_class('dw-t dw-depth-'.$depth); ?> id="li-comment-<?php comment_ID(); ?>">
+	<?php/*
+	Don't add `comment_class` to the <li>; add it to the <article> instead.
+	Otherwise Themes that color the background for .bypostauthor comments would paint
+   the whole <li> — which doesn't look nice, and is not needed,
+	with Debiki's two dimensional layout.
+	*/?>
+	<li <?php echo "class='dw-t dw-depth-$depth'" ?> id="li-comment-<?php comment_ID(); ?>">
 		<a class="dw-z">[–]</a>
-		<article id="comment-<?php comment_ID(); ?>" class="comment dw-p">
-			<footer class="comment-meta dw-p-hd">
-				<div class="comment-author vcard">
+		<article id="comment-<?php comment_ID(); ?>"
+				class="<?php echo debiki_comment_classes(), ' dw-p' ?>" >
+			<footer class="dw-p-hd">
+				<div>
 					<?php
-						$avatar_size = 68;
-						if ( '0' != $comment->comment_parent )
-							$avatar_size = 39;
-
-						echo get_avatar( $comment, $avatar_size );
-
-						/* translators: 1: comment author, 2: date and time */
-						printf( __( '%1$s on %2$s <span class="says">said:</span>', 'twentyeleven' ),
-							sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
-							sprintf( '<a href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
-								esc_url( get_comment_link( $comment->comment_ID ) ),
-								get_comment_time( 'c' ),
-								/* translators: 1: date, 2: time */
-								sprintf( __( '%1$s at %2$s', 'twentyeleven' ), get_comment_date(), get_comment_time() )
-							)
-						);
+					$avatar_size = $comment->comment_parent === '0' ? 69 : 39;
+					echo get_avatar($comment, $avatar_size);
 					?>
-
-					<?php edit_comment_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- .comment-author .vcard -->
+					<div class="dw-p-by"><?php comment_author_link() ?></div>
+					<?php/*
+					COULD find out which <time> class is "best"? `pubdate` or `published`?
+					*/?>
+					<time class='published' datetime='<?php get_comment_time('c') ?>'>
+						<?php comment_date() ?>
+					</time>
+				</div>
 
 				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentyeleven' ); ?></em>
-					<br />
+					<em class="comment-awaiting-moderation">
+						<?php _e( 'Your comment is awaiting moderation.', 'twentyeleven' ); ?>
+					</em>
 				<?php endif; ?>
-
 			</footer>
 
-			<div class="comment-content dw-p-bd"><?php comment_text(); ?></div>
+			<div class="dw-p-bd">
+				<?php comment_text() ?>
+			</div>
 
-			<div class="reply dw-wp-reply-link" <?php echo debiki_reply_link_data($comment); ?> >
-				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply <span>&darr;</span>', 'twentyeleven' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
-		</article><!-- #comment-## -->
+			<span class="dw-wp-actions">
+				<span class="dw-wp-reply-link" <?php echo debiki_reply_link_data($comment); ?> >
+					<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply <span>&darr;</span>', 'twentyeleven' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+				</span>
+				<span class="dw-wp-edit-link">
+					<?php edit_comment_link(
+							__( 'Edit', 'twentyeleven' ),
+							'<span class="edit-link">', '</span>' ); ?>
+				</span>
+				<?php /*
+				COULD include comment permalink:
+					esc_url( get_comment_link( $comment->comment_ID ) )
+				*/?>
+			</span>
+		</article>
 
 	<?php
 			break;
 	endswitch;
 }
-
 
 ?>
