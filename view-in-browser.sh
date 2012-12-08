0f0814d4dbf3a37a0f028ff77a5e8146ae1c6ad0
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-12-08 18:56:34 +0100

    Add more view-in-browser lines.

--- view-in-browser.sh
+++ view-in-browser.sh
@@ -4,10 +4,14 @@
 # for all (?) themes that Debiki supports. Also opens each
 # page with Debiki disabled, for comparison.
 
-google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Responsive'
-google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Responsive&debiki-comments-enabled=false'
-google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Twenty+Ten'
-google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Twenty+Ten&debiki-comments-enabled=false'
+#google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Responsive'
+#google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Responsive&debiki-comments-enabled=false'
+#google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Twenty+Ten'
+#google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Twenty+Ten&debiki-comments-enabled=false'
 google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Twenty+Eleven'
 google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Twenty+Eleven&debiki-comments-enabled=false'
+google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Annotum+Base'
+google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=Annotum+Base&debiki-comments-enabled=false'
+google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=The+Bootstrap'
+google-chrome 'http://192.168.122.83/2010/12/hello-world/?temp-theme=The+Bootstrap&debiki-comments-enabled=false'
 
