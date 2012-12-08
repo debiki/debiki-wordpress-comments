595b5aca14df591f472c7be81a357c47836641b8
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-12-08 18:22:09 +0100

    Use AngularJS test files in angular-seed submodule
    Don't use any copies I placed in e.g. test/lib/.

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -2,7 +2,9 @@
 
 BASE_DIR=`dirname $0`
 
-java -jar "$BASE_DIR/../test/lib/jstestdriver/JsTestDriver.jar" \
+java -jar "$BASE_DIR/../angular-seed/test/lib/jstestdriver/JsTestDriver.jar" \
      --config "$BASE_DIR/../config/jsTestDriver-scenario.conf" \
      --basePath "$BASE_DIR/.." \
      --tests all --reset
+
+     #--runnerMode DEBUG \
