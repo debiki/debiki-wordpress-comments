c62739f33c26bdab00fa8149b5e7eea71f59d85f
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-12-24 12:52:53 +0100

    Add licensing info to some files, change homepage.

--- test/php/CommentRatingsTest.php
+++ test/php/CommentRatingsTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+ * License: GPL v2 or later
+ */
+
 
 namespace Debiki\CommentRatingsTest;
 
--- theme-specific/twentyeleven-v0-comments.php
+++ theme-specific/twentyeleven-v0-comments.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+ * License: GPL v2 or later
+ */
 
 add_filter('debiki_comment_section_classes', function($classes) {
 	return $classes.' entry-content';
