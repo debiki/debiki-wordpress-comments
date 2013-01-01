8d322caf1cd82e60e500bd6ca04e66dec3c7ec0e
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-01-02 00:25:33 +0100

    Add unzip test utility script.

---
+++ scripts/unzip-dist-at-dotdot-unzipped.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Unzips build/debiki-wordpress-comments.zip
+# to wp-plugins/debiki-wordpress-comments-unzipped/
+# so that I can verify that the .zip bundle works okay.
+
+set -u  # exit on unset variable
+set -e  # exit on non-zero command exit code
+set -o pipefail  # exit on false | true
+
+name=debiki-wordpress-comments
+unzip_dir=$name-unzipped
+
+# Verify that we're located in
+#   wp-content/plugins/<name-of-debiki-wordpress-comments-dir>/.
+# (this script exits on non-zero exit code).
+ls ../../../wp-content/plugins >> /dev/null
+
+# Create wp-content/plugins/debiki-wordpress-comments-unzipped/.
+mkdir -p ../$unzip_dir
+
+# Unzip files.
+unzip build/$name.zip -d ../$unzip_dir/
+mv ../$unzip_dir/$name/* ../$unzip_dir/
+rmdir ../$unzip_dir/$name/
+
+# Rename the unzipped plugin, so it's possible to know which one
+# is from the Git repo, and which one is the unzipped version.
+sed 's/Plugin Name: Debiki for Wordpress/Plugin Name: Debiki for Wordpress UNZIPPED/' \
+   ../$unzip_dir/debiki-wordpress.php > \
+   ../$unzip_dir/debiki-wordpress.php-name-renamed
+mv ../$unzip_dir/debiki-wordpress.php-name-renamed \
+   ../$unzip_dir/debiki-wordpress.php
+
+echo "Unzipped plugin to here: ../$unzip_dir/
+and renamed it to: Debiki for Wordpress UNZIPPED"
+
